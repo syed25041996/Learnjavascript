d8450d6f60fdba455ea36b1def9c29fdc48b242c
syedmzensar <syed.m@zensar.com>
2022-11-16 18:39:07 +0530

    added styles

--- datetime/script.js
+++ datetime/script.js
@@ -5,7 +5,7 @@
 const setDate = () => {
     let date = new Date();
     
-    document.querySelector('.current-date').innerHTML =  date;
+    document.querySelector('.current-date').innerHTML =  date.toLocaleString();
     document.querySelector('.america-date').innerHTML =  date.toLocaleString('en-US', { timeZone: 'America/New_York' });
     setTimeout(setDate, 1000);
 }
